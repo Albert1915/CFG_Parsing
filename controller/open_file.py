f4e28e3de80b17792998b3898fbf7a2958a9bf3b
Albert Okario <76970766+Albert1915@users.noreply.github.com>
2022-12-22 00:21:58 +0800

    Update open_file.py

--- controller/open_file.py
+++ controller/open_file.py
@@ -1,9 +1,15 @@
+# open the cnf rule files
 def open_file(filepath):
-    data = [] # list of rules
+    # prepare the empty list
+    data = []
+    # open txt with read mode
     with open(filepath, 'r') as file:
-        raw = file.readlines() # read file
+        # read rule line by line
+        raw = file.readlines()
 
+        # append each rule into data list while remove the new line
         for rule in raw:
-            data.append(rule.strip('\n')) # append rule to list
+            data.append(rule.strip('\n'))
 
-    return data # return list of rules
+    # return the raw cnf rules
+    return data
