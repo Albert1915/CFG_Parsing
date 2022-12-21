6dd9e24fefd4b326c0b445bbd01f9edea4137ca9
Albert Okario <76970766+Albert1915@users.noreply.github.com>
2022-12-22 00:21:30 +0800

    Update open_file.py

--- controller/open_file.py
+++ controller/open_file.py
@@ -1,18 +1,9 @@
-# open the uploaded cnf rule file
-def open_file(uploaded_file):
-    # prepare the empty list
-    data = [] 
-    # if the file is not empty
-    if uploaded_file is not None:
-        # read the file
-        raw = uploaded_file.read().decode('utf-8').splitlines()
-        # remove the empty line
-        raw = list(filter(None, raw))
-            
+def open_file(filepath):
+    data = [] # list of rules
+    with open(filepath, 'r') as file:
+        raw = file.readlines() # read file
 
-        # append each rule into data list while remove the new line
         for rule in raw:
-            data.append(rule.strip('\n'))
+            data.append(rule.strip('\n')) # append rule to list
 
-    # return the raw cnf rules
-    return data
+    return data # return list of rules
