978349b27c408accbd2ab7911d4a2ac96409ee7b
Albert Okario <76970766+Albert1915@users.noreply.github.com>
2022-12-21 17:14:33 +0800

    Update raw_conversion.py

--- controller/raw_conversion.py
+++ controller/raw_conversion.py
@@ -1,29 +1,31 @@
 # convert raw rules into readable by Python
 def raw_to_cfg(raw):
-    # create list to store the rules
-    data = []
-    # for every rule in raw rules
-    for rule in raw:
-        # split the rule by space
-        rule = rule.split(' ')
-        # append the rule into data list
-        data.append(rule)
-    # return the data
-    return data
+    # prepare empty list
+    cfg = []
 
-# convert raw rules into html format
-def raw_to_html(raw):
-    # create list to store the rules
-    data = []
-    # for every rule in raw rules
-    for rule in raw:
-        # split the rule by space
-        rule = rule.split(' ')
-        # append the rule into data list
-        data.append(rule)
-    # return the data
-    return create_html(data)
+    # for each line in rules, split it by ' -> ', then append into cfg list
+    for line in raw:
+        cfg.append(line.split(' -> '))
 
+    # for each body in rules, split the body by ' | ' then convert into set
+    for rule in cfg:
+        rule[1] = set(rule[1].split(' | '))
+
+    # for each line in rules
+    for rule in cfg:
+        # prepare the empty set
+        new_body = set()
+        # for every element in body
+        for element in rule[1]:
+            # convert each body (variable and terminal) into tuple by splitting it by ' '
+            element_to_tuple = tuple(element.split(' '))
+            # append the tupple into new_body set
+            new_body.add(element_to_tuple)
+        # change the old body with new body format
+        rule[1] = new_body
+
+    # return the readable rules
+    return cfg
 
 # create rules in html format
 def create_html(raw):
