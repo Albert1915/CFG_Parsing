e92159e6f405d38fbaa935f6f29a10391e4f6e99
Albert Okario <76970766+Albert1915@users.noreply.github.com>
2022-12-29 13:12:41 +0800

    Update cyk_parse.py

--- controller/cyk_algorithm/cyk_parse.py
+++ controller/cyk_algorithm/cyk_parse.py
@@ -3,7 +3,7 @@ import streamlit as st
 from pandas import DataFrame
 
 # import the triangular table process
-from controller.cyk_algorithm.table_filling import *
+from controller.cyk_algorithm.table_fil import *
 
 # parse the sentence with cyk algorithm
 def parse(cnf, list_of_string):
